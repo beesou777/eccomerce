5ec3e548f462c4704816a4b4a1b2a8da2c299b14
beesou777 <sbeesou@gmail.com>
2023-05-31 08:50:14 +0545

    make order

--- controller/order.js
+++ controller/order.js
@@ -55,9 +55,58 @@ const createOrder = async (req,res)=>{
         res.status(500).json({error:"internal server error"})
     }
 }
+// find order y id
+
+const orderById = async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const orderId = await Order.findById(id)
+        if(!orderId){
+            return res.status(404).json({msg:"cannot get order"})
+        }
+        res.status(200).json({orderId})
+
+    } catch (error) {
+        console.log("error fetching order",error)
+        res.status(500).json({msg:"internal server error"})
+    }
+}
 
 // edit and patch order
-
+const updateOrder = async (req,res)=>{
+    const {id} = req.params
+    const {
+        total_amount,
+        discounted_price,
+        order_status,
+        payment_type,
+        delivery_amount,
+        order_items,
+        order_datetime,
+        product_id,product_image,quantity,price,product_name
+      } = req.body;
+      try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+            id,{
+                total_amount,
+                discounted_price,
+                order_status,
+                payment_type,
+                delivery_amount,
+                order_items,
+                order_datetime,
+                product_id,
+                product_image,
+                quantity,
+                price,product_name
+            },
+            {new:true})
+            res.status(200).json({order:updatedOrder})
+      } catch (error) {
+        console.log("error updating the order",error)
+        res.status(500).json({error:"internal server error"})
+      }
+}
 // delete order
 
-module.exports = {getAllOrder,createOrder}
+module.exports = {getAllOrder,createOrder,updateOrder,orderById}
--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router();
 
-const { getAllOrder, createOrder } = require("../controller/order");
+const { getAllOrder, createOrder,updateOrder,orderById } = require("../controller/order");
 router.route("/")
   .get(getAllOrder)
   .post(createOrder);
 
+
+router.route("/:id").put(updateOrder).patch(updateOrder).get(orderById)
 module.exports = router
