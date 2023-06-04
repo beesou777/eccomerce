bf4e5afba9ab28839065cb6a4e96e9f8c3a4f853
beesou777 <sbeesou@gmail.com>
2023-06-04 13:54:02 +0545

    commit

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/upload', express.static('upload'));
-// Enable CORS
+
+// Enable CORS for all origins
 app.use(cors());
 
 // Set routes
